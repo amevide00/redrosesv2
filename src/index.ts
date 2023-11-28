4902dcbecb584bef4bac4e2fd6b60ca5234b0a9a
amevide998 <hdsvidi@gmail.com>
2023-11-29 01:31:59 +0700

    test deploy vercel

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { greeting } from './text';
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import { development, production } from './core';
 
-const BOT_TOKEN = process.env.BOT_TOKEN || '';
+const BOT_TOKEN = '6464929601:AAED3OMfRzoJ3oq5u9As7bajl-4q6EVepb0' || '';
 const ENVIRONMENT = process.env.NODE_ENV || '';
 
 const bot = new Telegraf(BOT_TOKEN);
