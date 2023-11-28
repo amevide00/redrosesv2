00bf94402b7e36a9e27d152e0d4e840fa48fb7ba
amevide998 <hdsvidi@gmail.com>
2023-11-29 01:41:00 +0700

    debug

--- src/commands/about.ts
+++ src/commands/about.ts
@@ -6,6 +6,7 @@ import { author, name, version } from '../../package.json';
 const debug = createDebug('bot:about_command');
 
 const about = () => async (ctx: Context) => {
+  console.log('about in', ctx)
   const message = `*${name} ${version}*\n${author}`;
   debug(`Triggered "about" command with message \n${message}`);
   await ctx.replyWithMarkdownV2(message, { parse_mode: 'Markdown' });
